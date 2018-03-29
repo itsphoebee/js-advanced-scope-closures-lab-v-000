9f093fc6884181bc71ef5008d0107a8f0ac907cb
Phoebe Lau <phoebelau7@gmail.com>
2018-03-29 04:02:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(range){
   return function(startingLocation, endingLocation){
     if (parseInt(endingLocation) - parseInt(startingLocation) <= parseInt(range)){
-      return `within range by ${endingLocation - startingLocation}`
+      return `within range by ${parseInt(endingLocation) - parseInt(startingLocation)}`
     }
   }
 }
