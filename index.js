aa3bced63cc196e2a7a171bbc90ef03ed8377982
Phoebe Lau <phoebelau7@gmail.com>
2018-03-29 04:08:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(range){
   return function(startingLocation, endingLocation){
     let totalBlocks = Math.abs(parseInt(endingLocation) - parseInt(startingLocation))
     if (totalBlocks <= range){
-      return `within range by ${totalBlocks - range}`;
+      return `within range by ${Math.abs(totalBlocks - range)}`;
     } else {
       return `${totalBlocks - range} blocks out of range`;
     }
